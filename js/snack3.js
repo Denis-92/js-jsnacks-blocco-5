1eff3076d97a1e0fc786e069cf87790976d377bc
Denis-92 <d.mozenic@gmail.com>
2022-09-09 16:48:06 +0200

    aggiunto snack 1 e 2

---
+++ js/snack3.js
@@ -0,0 +1,8 @@
+/* SNACK 3*/
+/*
+ * Crea 10 oggetti che rappresentano una zucchina.
+ * Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+ * Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+ */
+
+// CREO UN ARRAY CON 10 OBJECT (CIASCUN OBJECT HA UN CAMPO CONTENENTE LA LUNGHEZZA)
